898e795b6f0933b1cdbfc68637b988dbf39537dc
hozuki <meta.maetel@gmail.com>
2019-09-02 07:52:16 +0900

    完成。

---
+++ Sakuraso/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Hankakuyasan
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("imm32.dll")]
+        static extern IntPtr ImmGetDefaultIMEWnd(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern bool GetGUIThreadInfo(uint dwthreadid, ref GUITHREADINFO lpguithreadinfo);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct GUITHREADINFO
+        {
+            public int cbSize;
+            public int flags;
+            public IntPtr hwndActive;
+            public IntPtr hwndFocus;
+            public IntPtr hwndCapture;
+            public IntPtr hwndMenuOwner;
+            public IntPtr hwndMoveSize;
+            public IntPtr hwndCaret;
+            public System.Drawing.Rectangle rcCaret;
+        }
+
+        const int WM_IME_CONTROL = 0x283;
+        const int IMC_SETCONVERSIONMODE = 2;
+        const int IMC_GETOPENSTATUS = 5;
+        private static void Execute()
+        {
+            GUITHREADINFO gti = new GUITHREADINFO();
+            gti.cbSize = Marshal.SizeOf(gti);
+
+            if (!GetGUIThreadInfo(0, ref gti))
+            {
+                Console.WriteLine("Failed To Run Rose");
+            }
+            IntPtr imwd = ImmGetDefaultIMEWnd(gti.hwndFocus);
+
+            bool imeEnabled = (SendMessage(imwd, WM_IME_CONTROL, (IntPtr)IMC_GETOPENSTATUS, IntPtr.Zero) != 0);
+
+            if (imeEnabled)
+            {
+                SendMessage(imwd, WM_IME_CONTROL, (IntPtr)IMC_SETCONVERSIONMODE, (IntPtr)0);
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Execute();
+        }
+    }
+}
